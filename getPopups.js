9ae71d06fe5cd754b077ae8fcd5cacc106f147f8
wenyaoxue <crystalywen26@gmail.com>
2026-09-01 22:29:51 -0500

    double index adjust

--- getPopups.js
+++ getPopups.js
@@ -39,17 +39,12 @@ function createTr(questionIndex, deckIndex) {
     let tr = document.createElement("tr")
 
     let ind = document.createElement("td")
-    ind.innerHTML = "<a onclick='closepopup(); drawNewCard("+deckIndex+");'>" + (deckIndex + 1) + "</a>"
-
-    let id = document.createElement("td")
-    id.style.fontSize = "xx-small"
-    id.innerHTML = ((questionIndex == deckIndex) ? " " : (questionIndex + 1))
+    ind.innerHTML = "<a onclick='closepopup(); drawNewCard("+deckIndex+");'>" + (deckIndex + 1) + "</a><br><span style='font-size:small'>" + ((questionIndex == deckIndex) ? " " : (questionIndex + 1)) + "</span>"
 
     let qust = document.createElement("td")
     qust.innerHTML = completequestions[questionIndex][0]
     qust.classList.add(completequestions[questionIndex][1])
     tr.appendChild(ind)
-    tr.appendChild(id)
     tr.appendChild(qust)
     return tr
 }
@@ -60,7 +55,7 @@ function popupViewed() {
     let table = document.createElement("table")
 
     let history = viewedIndex.filter((viewedIndex) => viewedIndex[0]).map((viewedIndex) => viewedIndex[1])
-    table.innerHTML = "<tr><th colspan='3'>" + (history.length == 0 ? "No deck history." : "Deck history") + "</th></tr>"
+    table.innerHTML = "<tr><th colspan='2'>" + (history.length == 0 ? "No deck history." : "Deck history") + "</th></tr>"
 
     history.forEach(
         (viewedI) => { table.appendChild(createTr(subdeckIndexes[viewedI], viewedI)) }
@@ -80,7 +75,7 @@ function popupAll() {
     let mod = document.getElementById("deckmodtext").innerHTML
     let cat = document.getElementById("deckcattext").innerHTML
     upperDeck = (mod=="" ? "" : (upperCase(mod) + " ")) + upperCase(cat)
-    table.innerHTML = "<tr><th colspan='3' class='" + cat + "'>" + (subdeckIndexes.length == 0 ? "No cards in deck." : upperDeck + " Deck") + "</th></tr>"
+    table.innerHTML = "<tr><th colspan='2' class='" + cat + "'>" + (subdeckIndexes.length == 0 ? "No cards in deck." : upperDeck + " Deck") + "</th></tr>"
     subdeckIndexes.forEach(
         (questionIndex, i) => { table.appendChild(createTr(questionIndex, i)) }
     )
