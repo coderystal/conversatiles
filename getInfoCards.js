67ec0744f9ba8cb5f281c477279130cec4bf9b2b
wenyaoxue <crystalywen26@gmail.com>
2026-09-01 23:21:31 -0500

    buy me a coffee image

--- getInfoCards.js
+++ getInfoCards.js
@@ -11,7 +11,8 @@ function infoCardCleanup() {
 function showTitle() {
     let thisdeck = document.getElementById("decktext").innerText
     let cat = document.getElementById("deckcattext").innerHTML.replace("!","")
-    document.getElementById("question").innerHTML = "<button class='coffee' onclick='event.stopPropagation()' style='display: none'><a href='https://buymeacoffee.com/jeyc35gujd' target='_blank' style='text-decoration: none;'>Buy me a coffee</a></button>" +
+    document.getElementById("question").innerHTML = "<button class='coffee' onclick='event.stopPropagation()' style='display: none'><a href='https://buymeacoffee.com/jeyc35gujd' target='_blank' style='text-decoration: none;'>"+
+        "<img id='bmacimg' src='images/bmacoffee.png'>Buy me a coffee</a></button>" +
         "<div class='cardq' style='width:100%;line-height:100%;margin:auto;font-size:5vh'><b>" +
         "Conversatiles</b><div style='font-size:2.5vh'>by coderystal</div>" +
         "</div>" +
